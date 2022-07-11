a8d8e6e97d4146eac423d67a97b840016acbe871
gyouza <sebirabadinc@gmail.com>
2022-07-11 18:42:05 -0400

    creating sagas

---
+++ budget/src/sagas/testSaga.js
@@ -0,0 +1,15 @@
+import {delay, take, put} from 'redux-saga/effects';
+export function* testSaga(){
+    while(true){
+        console.log("Starting saga");
+    const state = yield take('TEST_MESSAGE');
+    console.log("Finish sage function", state);
+    }
+}
+
+export function* dispatchTest() {
+    while(true) {
+        yield delay(1000);
+        yield put({type: 'TEST_MESSAGE', payload: 1000});
+    }
+}
--- budget/src/store/configureStore.js
+++ budget/src/store/configureStore.js
@@ -1,16 +1,23 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import entriesReducer from '../reducers/entries.reducers';
 import { composeWithDevTools } from '@redux-devtools/extension';
 import modalsReducer from '../reducers/modals.reducers';
+import createSagaMiddleware from 'redux-saga';
+import { initSagas } from "../sagas";
+
+const sagaMiddleware = createSagaMiddleware();
+const middlewares = [sagaMiddleware];
 
 
 const configureStore = () => {
-   return createStore (combineReducers ({
+   const store = createStore (combineReducers ({
     entries: entriesReducer,
     modals: modalsReducer,
   }),
-  composeWithDevTools()
+  composeWithDevTools(applyMiddleware(...middlewares))
   );
+  initSagas(sagaMiddleware);
+  return store
 };
 
 export default configureStore;
